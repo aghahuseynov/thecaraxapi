8813628b103217825fc4f8d37349c7c9decaec07
Aga Huseynov <agahuseynov@hotmail.com>
2019-07-10 00:36:30 +0300

    aa

---
+++ Services/Migrations/20190709212945_pre1.0.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Services.Migrations
+{
+    public partial class pre10 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BloodGroup",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Customers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Profession",
+                table: "Customers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BloodGroup",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Profession",
+                table: "Customers");
+        }
+    }
+}
